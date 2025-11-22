143bd881ce6347a145a04cd78daa10698e57aa2b
Leonardo Francisco Da Silva <leozinho24680@gmail.com>
2025-11-22 09:13:57 -0300

    terminal

---
+++ sintaxe-java/src/edu/leonardo/terminalArgumentos/MinhaClasse.java
@@ -0,0 +1,8 @@
+package edu.leonardo.terminalArgumentos;
+
+public class MinhaClasse {
+    public static void main(String[] args) {
+        System.out.println("Ola mundo, eu fui executado via-terminal");
+        
+    }
+}
